e00de33afe6751970a92f71d49115767b9226818
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-07-27 07:06:37 -0600

    added logic to display 5 day data

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ A tool to quickly get the weather forecast for major US cities
 
 MOCKUP
 ![mockup](https://github.com/tmorgan-dev/weather-dashboard/assets/132379127/df86f6ce-b090-4482-ae18-ec8983777f68)
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, 
+// the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,9 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, 
-// the temperature, the humidity, and the the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 var apiKey = "428568c242a23aec5b8d74156afd47b7"
 var city;
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?"
 
 var searchButton = document.querySelector("#search")
 var searchEl = document.querySelector("#searchfield")
-
 var resultContentEl = document.querySelector('#result-content')
 
 function search() {
@@ -36,12 +24,15 @@ function search() {
                 wind: data.wind.speed,
             }
             var time = dayjs(data.UtcTime).format('MMMM D, YYYY')
+            
             var resultCard = document.createElement('div')
             resultCard.classList.add('card')
 
             var resultBody = document.createElement('div');
             resultBody.classList.add('card-body');
             resultCard.append(resultBody);
+
+            resultBody.value = ""
 
             var bodyContentName = document.createElement('p');
             var bodyContentDate = document.createElement('p');
@@ -52,10 +43,13 @@ function search() {
 
             bodyContentName.textContent = "Name: " + weatherData.name
             bodyContentDate.textContent = "Date: " + time
-            bodyContentTemp.textContent = "Temp: " + weatherData.temp
-            bodyContentHumidity.textContent = "Humidity: " + weatherData.humidity
-            bodyContentWind.textContent = "Wind Speed: " + weatherData.wind
+            bodyContentTemp.textContent = "Temp: " + weatherData.temp + " °F"
+            bodyContentHumidity.textContent = "Humidity: " + weatherData.humidity + " %"
+            bodyContentWind.textContent = "Wind Speed: " + weatherData.wind + " MPH"
             bodyContentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherData.icon + "@2x.png")
+
+            // resultCard.value = ""
+            
 
             resultBody.append(bodyContentName);
             resultBody.append(bodyContentDate);
@@ -65,6 +59,62 @@ function search() {
             resultBody.append(bodyContentIcon);
 
             resultContentEl.append(resultCard);
+
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+
+            fetch(
+                `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+            ).then(function (response) {
+                return response.json()
+            }).then(function (fiveDayData) {
+                var fiveDayArray = fiveDayData.list
+                console.log(fiveDayArray)
+                var fiveDaySection = document.querySelector('#fivedaysection')
+                var fiveDayEl = ""
+                for (var i = 0; i <= 4; i++) {
+                    var dayOfTheWeek = new Date(fiveDayArray[i].dt_txt).toLocaleString().split(",")[0]
+                    var iconCode = fiveDayArray[i].weather[0].icon
+                    var iconImage = `<img src="http://openweathermap.org/img/wn/${iconCode}.png">`
+                    fiveDayEl += `
+                    <div class='fivedaycard'>
+                        <p>Date: ${dayOfTheWeek}</p>
+                        <p>${iconImage}</p>
+                        <p>Temp: ${fiveDayArray[i].main.temp} °F</p>
+                        <p>Humidity: ${fiveDayArray[i].main.humidity} %</p>
+                        <p>Wind Speed: ${fiveDayArray[i].wind.speed} MPH</p>
+                    </div>`;
+                    fiveDaySection.innerHTML = fiveDayEl
+                }
+            })
         })
+
+    history()
 }
+
+function history() {
+    var historyValue = searchEl.value.trim()
+    var storage = JSON.parse(localStorage.getItem("storage")) || []
+    storage.push(historyValue)
+    localStorage.setItem("storage", JSON.stringify(storage))
+    createbuttons(storage)
+}
+function createbuttons(storage) {
+    var historySection = document.querySelector('#historysection')
+    historySection.innerHTML = ""
+    storage.forEach(function (city) {
+        var cityList = document.createElement("li")
+        cityList.textContent = city
+        cityList.className += "searched-city"
+        historySection.appendChild(cityList)
+
+        cityList.addEventListener("click", function (event) {
+            event.preventDefault()
+            var oldCitySearch = cityList.textContent
+            search(oldCitySearch)
+        })
+    })
+}
+
 searchButton.addEventListener("click", search)
+
